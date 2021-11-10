145c7ac3a95d9dd4e8ba35caa760e099e5cba72d
ehf <me@ehf.im>
2021-11-10 11:20:27 -0800

    Update rpm_db_runrecovery.sh

--- rpm_db_runrecovery.sh
+++ rpm_db_runrecovery.sh
@@ -3,13 +3,15 @@
 DATE=$(date +%Y%m%d.%H%M%S)
 timeout --signal=9 20s rpm -qa 2>&1 | grep -qE 'DB_RUNRECOVERY: Fatal error, run database recovery'
 if [[ $? -eq 0 ]]; then
-   fuser -v /var/lib/rpm/__db.[[:digit:]]* 2>&1 | grep -qE [[:alnum:]]
+   ##fuser -v /var/lib/rpm/__db.[[:digit:]]* 2>&1 | grep -qE [[:alnum:]]
+   fuser -v /var/lib/rpm/ 2>&1 | grep -qE [[:alnum:]]
    if [[ $? -eq 1 ]]; then
       mkdir /var/tmp/rpm-db-backup-${DATE}
       mv /var/lib/rpm/__db* /var/tmp/rpm-db-backup-${DATE}
       yum clean all && rpm --rebuilddb
    else
-      fuser -v /var/lib/rpm/__db.[[:digit:]]* 2>&1 | tee /var/tmp/rpmdb-fuser-check-${DATE}.log
+      ##fuser -v /var/lib/rpm/__db.[[:digit:]]* 2>&1 | tee /var/tmp/rpmdb-fuser-check-${DATE}.log
+      fuser -v /var/lib/rpm/ 2>&1 | tee /var/tmp/rpmdb-fuser-check-${DATE}.log
    fi
 else
    printf "DB_RUNRECOVERY error not found. Will not run 'rpm --rebuilddb'.\n"
