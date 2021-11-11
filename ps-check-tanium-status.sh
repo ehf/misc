9d5016b4b782200fccfddd55f9e71fe337ba9b19
ehf <me@ehf.im>
2021-11-10 21:37:11 -0800

    Update ps-check-tanium-status.sh

--- ps-check-tanium-status.sh
+++ ps-check-tanium-status.sh
@@ -13,8 +13,9 @@ exit 0
 
 
 ### example output
-#    h100.d.408.systems 2.6.32-754.41.2.el6.x86_64 18:17:11_up_7_days 19 PRESENT
-#    h101.d.408.systems 2.6.32-754.41.2.el6.x86_64 18:17:11_up_7_days 22 PRESENT
-#    h102.d.408.systems 2.6.32-754.41.2.el6.x86_64 18:17:11_up_7_days 22 PRESENT
-#    h103.d.408.systems 2.6.32-754.41.2.el6.x86_64 18:17:11_up_7_days 21 PRESENT
-#    h104.d.408.systems 2.6.32-754.39.1.el6.x86_64 18:17:11_up_155_days 22 PRESENT
+#    h100.d.408.systems,2.6.32-754.41.2.el6.x86_64,18:17:11_up_7_days,19,PRESENT
+#    h101.d.408.systems,2.6.32-754.41.2.el6.x86_64,18:17:11_up_7_days,22,PRESENT
+#    h102.d.408.systems,2.6.32-754.41.2.el6.x86_64,18:17:11_up_7_days,22,PRESENT
+#    h103.d.408.systems,2.6.32-754.41.2.el6.x86_64,18:17:11_up_7_days,21,PRESENT
+#    h104.d.408.systems,2.6.32-754.39.1.el6.x86_64,18:17:11_up_155_days,22,PRESENT
+#
