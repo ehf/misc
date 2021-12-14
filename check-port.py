41006a97f04680fe70a35b6633c1dc1a609021a9
ehf <1011013+ehf@users.noreply.github.com>
2021-12-14 08:41:22 -0800

    Update check-port.py

--- check-port.py
+++ check-port.py
@@ -5,7 +5,7 @@ import socket
 
 a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-location = ("sc1tanium01.csz.ariba.com", 17472)
+location = ("host.example.com", 17472)
 result_of_check = a_socket.connect_ex(location)
 
 if result_of_check == 0:
